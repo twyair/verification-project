4558455dbbafe6b1532ae70b3bd478d3676be25e
twyair <yairt16@gmail.com>
2021-05-12 07:50:16 +0300

    use a phantom proof in `binary_search()`

--- benchmarks/random.c
+++ benchmarks/random.c
@@ -224,8 +224,28 @@ int mccarthy_91(int n) {
 int binary_search(int arr[], int size, int key) {
     requires(
         size > 0
-        && forall(k, range(0, size), forall(t, range(0, k + 1), arr[t] <= arr[k]))
+        && forall(k, range(0, size - 1), arr[k] <= arr[k + 1])
     );
+    phantom({
+        remember(
+            size > 0
+            && forall(k, range(0, size - 1), arr[k] <= arr[k + 1])
+        );
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j >= 0; j--) {
+                assert(
+                    forall(k, range(0, i), forall(t, range(0, k + 1), arr[t] <= arr[k]))
+                    && forall(t, range(j, i + 1), arr[t] <= arr[i])
+                    && i < size && i >= 0
+                    && j >= 0 && j <= i
+                );
+            }
+            assert(
+                forall(k, range(0, i + 1), forall(t, range(0, k + 1), arr[t] <= arr[k]))
+                && i < size && i >= 0
+            );
+        }
+    });
     remember(
         size > 0
         && forall(k, range(0, size), forall(t, range(0, k + 1), arr[t] <= arr[k]))
