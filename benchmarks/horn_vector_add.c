55e43634cc854bf4227d6a296b6e4ba5cd940a87
twyair <yairt16@gmail.com>
2021-07-19 15:56:49 +0300

    m

--- benchmarks/horn_vector_add.c
+++ benchmarks/horn_vector_add.c
@@ -2,8 +2,8 @@
 
 void vector_add(int v[], int u[], int res[], int size) {
     requires(size > 0);
+    ensures(forall(k, range(0, size), res[k] == v[k] + u[k]));
     for (int i = 0; i < size; i++) {
         res[i] = v[i] + u[i];
     }
-    assert(forall(k, range(0, size), res[k] == v[k] + u[k]));
 }
